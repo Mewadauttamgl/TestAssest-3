aaef3b35b6ce706b281e8a86810d717e91a8ce4f
Mewadauttamgl <110605686+Mewadauttamgl@users.noreply.github.com>
2022-08-22 10:45:07 +0530

    Add files via upload

---
+++ Test_3-Q-2/dao/CustomerRepo.java
@@ -0,0 +1,11 @@
+package com.example.demo.dao;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.name.demo.model.Customer;
+
+public interface CustomerRepo extends JpaRepository<Customer, Integer> 
+{
+
+}
+
---
+++ Test_3-Q-2/data.sql
@@ -0,0 +1,5 @@
+insert into customer values(101,'Uttam','Indore','Uttam@123','UttamMewada987');
+insert into customer values(102,'Shivam','Bhopal','Shivam@123','ShivamMewada876');
+insert into customer values(103,'Utkarsh','Jabalpur','Utkarsh@123','UtkarshAwasthi765');
+insert into customer values(104,'Yash','Sehore','Yash@123','YashSharma654');
+insert into customer values(105,'Aniket','Shujalpur','Aniket@123','AniketTiwari543');

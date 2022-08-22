29070ac90f83e671acdf75f635ba5c786e272d4e
Mewadauttamgl <110605686+Mewadauttamgl@users.noreply.github.com>
2022-08-22 10:51:43 +0530

    Update CustomerRepo.java

--- Test_3-Q-2/dao/CustomerRepo.java
+++ Test_3-Q-2/dao/CustomerRepo.java
@@ -2,7 +2,7 @@ package com.example.demo.dao;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.name.demo.model.Customer;
+import com.example.demo.model.Customer;
 
 public interface CustomerRepo extends JpaRepository<Customer, Integer> 
 {

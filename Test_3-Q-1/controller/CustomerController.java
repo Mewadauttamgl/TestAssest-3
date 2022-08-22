e942a2a8edf4f2e072071befe1944dfe194fb653
Mewadauttamgl <110605686+Mewadauttamgl@users.noreply.github.com>
2022-08-22 10:25:20 +0530

    Add files via upload

---
+++ Test_3-Q-1/controller/CustomerController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.springframework.data.annotation.Reference;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class CustomerController {
+	@RequestMapping("/")
+	public String home() {
+		
+		return "home.jsp";
+	}
+	@RequestMapping("/hello")
+	public String showCustomer(@RequestParam("c_id") int customer_Id,@RequestParam("c_name") String customer_Name,@RequestParam("c_phone") int customer_Phone,@RequestParam("c_addr") String customer_Address,@RequestParam("c_logid") String customer_loginId,@RequestParam("c_pass") int customer_Password,Model m) {
+		m.addAttribute("c_id",customer_Id);
+		m.addAttribute("c_name",customer_Name);
+		m.addAttribute("c_phone",customer_Phone);
+		m.addAttribute("c_addr",customer_Address);
+		m.addAttribute("c_logid",customer_loginId);
+		m.addAttribute("c_pass",customer_Password);
+		
+		
+		return "display.jsp";
+		
+		
+		
+	}
+
+}
---
+++ Test_3-Q-1/webapp/view/display.jsp
@@ -0,0 +1,22 @@
+<html>
+<body>
+<h1>Your details ${c_name}.....</h1><br> <br>
+
+Id :- ${c_id}<br><br>
+
+
+
+PhoneNumber:- ${c_phone}<br><br>
+
+Address:- ${c_addr} <br><br>
+
+LoginId:- ${c_logid}<br><br>
+
+
+<h2>thanks for registration ${c_name }</h2>
+
+
+</body>
+
+
+</html>
